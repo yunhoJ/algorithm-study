cf8eb3c427133879cbcb99435cadd89ca316beb9
전윤호 <86466096+yunhoJ@users.noreply.github.com>
2025-08-07 23:11:24 +0900

    [Silver II] Title: 스택 수열, Time: 288 ms, Memory: 27472 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> integerStack = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
+        int num = 1;
+        for (int i = 0; i <N ; i++) {
+            int a = Integer.parseInt(br.readLine());
+
+            while (a>=num){
+                integerStack.push(num);
+                sb.append("+\n");
+//                if (a==num){
+//                    integerStack.pop();
+//                    sb.append("-\n");
+//                }
+                num+=1;
+            }
+
+            if (integerStack.pop()==a){
+                sb.append("-\n");
+            }else{
+                sb.setLength(0);
+                sb.append("NO");
+                break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
