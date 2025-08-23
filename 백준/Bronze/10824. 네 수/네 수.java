197704e1817b6d9cdcada682e720e3d7ed3ac115
전윤호 <86466096+yunhoJ@users.noreply.github.com>
2025-08-23 16:35:46 +0900

    [Bronze III] Title: 네 수, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Long.parseLong(st.nextToken()+st.nextToken())+Long.parseLong(st.nextToken()+st.nextToken()));
+    }
+}
