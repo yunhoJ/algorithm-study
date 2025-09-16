e43be761f954a92e2e46762ce942d65ecb59ec51
전윤호 <86466096+yunhoJ@users.noreply.github.com>
2025-09-17 00:14:07 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 124 ms, Memory: 14552 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main{
+    static  int pay [];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        pay = new int[N+1];
+        Arrays.fill(pay,100000);
+        pay[0] = 0;
+        int[] card_pay = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i <N ; i++) {
+            card_pay[i]=Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i <=N ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                pay[i] = Math.min(pay[i],card_pay[i-j]+pay[j-1]);
+            }
+        }
+        System.out.println(pay[N]);
+    }
+}
