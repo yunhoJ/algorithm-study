1ab73da9e6a01913a2a047677837c465f387bcde
전윤호 <86466096+yunhoJ@users.noreply.github.com>
2025-09-16 23:58:27 +0900

    [Silver I] Title: 카드 구매하기, Time: 124 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,22 +1,23 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 class Main{
-    private static int[] money;
-
+    static  int pay [];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter( new OutputStreamWriter(System.out));
-        int N=Integer.parseInt(br.readLine());
-        String []arr =br.readLine().split(" ");
-        money = new int[N+1];
-        money[0] = 0;
-        for (int i = 1; i <=N ; i++) {
-            for (int j = 1; j <=i; j++) {
-                money[i] = Math.max(money[i], money[i-j] + Integer.parseInt(arr[j-1]));
 
+        int N = Integer.parseInt(br.readLine());
+        pay = new int[N+1];
+        int[] card_pay = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i <N ; i++) {
+            card_pay[i]=Integer.parseInt(st.nextToken());
+        }
+        for (int i = 1; i <=N ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                pay[i] = Math.max(pay[i],card_pay[i-j]+pay[j-1]);
             }
         }
-        System.out.println(money[N]);
+        System.out.println(pay[N]);
     }
 }
-
