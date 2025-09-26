0a8d3ebfd7e1447fc55b9e4f87271cc3686fcedd
전윤호 <86466096+yunhoJ@users.noreply.github.com>
2025-09-27 00:17:22 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.Arrays;
+
+
+class Main {
+    static long pay[][] = new long[92][2];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        pay[1][0] = 0;
+        pay[1][1] = 1;
+
+        pay[2][0] = 1;
+        pay[2][1] = 0;
+
+        System.out.println(dp(N,-1));
+    }
+
+    private static long dp(int n,int ze) {
+        if (pay[n][0] + pay[n][1] != 0) {
+            if (ze==-1){
+                return pay[n][0] + pay[n][1];
+            }
+            else if (ze==0){
+                return pay[n][0];
+            }else return pay[n][1];
+
+        }
+        pay[n][1] = dp(n-1,0) ;
+        pay[n][0] = dp(n-1,1) + dp(n-1,0);
+        if (ze==-1){
+            return pay[n][0] + pay[n][1];
+        }
+        else if (ze==0){
+            return pay[n][0];
+        }else return pay[n][1];
+
+    }
+}
+
+
+
+
