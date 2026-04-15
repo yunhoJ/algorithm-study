fee37970c8b9b1b85b38f8c671a52e2ce8d407bb
전윤호 <86466096+yunhoJ@users.noreply.github.com>
2026-04-15 19:06:47 +0900

    [Bronze V] Title: 시험 성적, Time: 216 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        switch (a/10){
+            case 10:
+                System.out.println("A");
+                break;
+            case 9:
+                System.out.println("A");break;
+            case 8:
+                System.out.println("B");break;
+            case 7:
+                System.out.println("C");break;
+            case 6:
+                System.out.println("D");break;
+            default:
+                System.out.println("F");
+        }
+    }
+}
