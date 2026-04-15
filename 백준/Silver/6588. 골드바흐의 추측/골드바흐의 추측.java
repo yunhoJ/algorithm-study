4361c9c5c1f3a3eb0a5212a4857b73f24d612662
전윤호 <86466096+yunhoJ@users.noreply.github.com>
2026-04-15 19:06:26 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1136 ms, Memory: 312840 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static Map<Integer, Boolean> primeCache = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true){
+            int tmp=Integer.parseInt(br.readLine());
+            if (tmp==0){
+                break;
+            }
+            boolean checker = false;
+            for (int i = 3; i <= tmp / 2; i += 2) { // 절반까지만 확인
+                if (is_prime(i) && is_prime(tmp - i)) {
+                    sb.append(tmp).append(" = ").append(i).append(" + ").append(tmp - i).append("\n");
+                    checker = true;
+                    break;
+                }
+            }
+            if (!checker){
+                sb.append("Goldbach's conjecture is wrong.\n");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static boolean is_prime(int i) {
+        if (primeCache.containsKey(i)){
+            return primeCache.get(i);
+        }
+        if (i<2){
+            return false;
+        }
+        for (int j = 2; j <= Math.sqrt(i) ; j++) {
+            if(i%j==0){
+                primeCache.put(i, false);
+                return false;
+            }
+        }
+        primeCache.put(i,true);
+        return true;
+    }
+}
